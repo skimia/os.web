2544f7bcef64aabae9c0b02bed5d605339b49622
the Dr3am <pixmobiles@gmail.com>
2017-02-19 18:49:33 +0100

    Creation de la partie static du dashboard

---
+++ app/plugins/os-dashboard/gulpfile.js
@@ -0,0 +1,45 @@
+var prefixerConfig = {
+    browsers: ['last 10 versions']
+};
+
+var sassConfig = {
+    onError: console.error.bind(console, 'SASS Error:'),
+    removeComments: true
+};
+
+function on_error(err){
+    console.log(err);
+    this.emit("end");
+}
+
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var count =     require('gulp-count');
+var prefixer =  require('gulp-autoprefixer');
+var livereload = require('gulp-livereload');
+
+gulp.task('default', function(){
+
+    gulp.src(['assets/scss/**/*.scss'])
+        .pipe(sass(sassConfig).on('error', sass.logError))
+        .pipe(prefixer(prefixerConfig))
+        .pipe(count())
+        .pipe(gulp.dest('assets/css/')).on('error',on_error);
+
+
+});
+
+gulp.task('run', function(){
+
+    livereload.listen();
+
+    gulp.watch(['assets/scss/**/*.scss'], ['default']);
+
+    gulp.watch(['assets/scss/**/*.scss','**/*.html'])
+        .on('change', function(event){
+            livereload.changed(event.path);
+        }).on('error',on_error);
+
+});
