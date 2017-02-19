539c1f34b2fa0897b24025224b9464a2aadf4296
the Dr3am <pixmobiles@gmail.com>
2017-02-19 20:27:23 +0100

    responsive dashboard

--- app/plugins/os-dashboard/controllers/dashboard_ctrl.js
+++ app/plugins/os-dashboard/controllers/dashboard_ctrl.js
@@ -5,6 +5,20 @@ os.module('os.dashboard').controller('dashboard_ctrl',function(){
         margin:20,
         nav:true,
         dotsEach: true,
-        navText :['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>']
+        navText :['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+        responsive : {
+
+            0 : {
+                items : 1
+            },
+
+            980 : {
+                items : 2
+            },
+
+            1024 : {
+                items :4
+            }
+        }
     });
 });
